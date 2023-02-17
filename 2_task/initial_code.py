906095b98a80005b6cb36e106bf861844aca481a
misterD35 <122601504+misterD35@users.noreply.github.com>
2023-02-17 11:03:55 +0300

    Delete initial_code.py

--- 2_task/initial_code.py
+++
@@ -1,9 +0,0 @@
-def create_handlers(callback):
-    handlers = []
-    for step in range(5):
-        handlers.append(lambda: callback(step))
-
-    def execute_handlers():
-        for handler in handlers:
-            handler()
-    return execute_handlers
